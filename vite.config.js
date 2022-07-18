a1a29230d576387de6d18063d3a62aa7f10bcbc1
Tinnei Pang <tinneipang@gmail.com>
2022-07-18 14:23:20 +0100

    added vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src'),
+      '@root': path.resolve(__dirname, './'),
+      '@public': path.resolve(__dirname, './public'),
+      '@cypress': path.resolve(__dirname, './cypress'),
+      '@dist': path.resolve(__dirname, './dist'),
+    },
+  }
 })
