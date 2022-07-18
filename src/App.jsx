edd2dab4d449a2fb26e54d786570b998c41a739a
Tinnei Pang <tinneipang@gmail.com>
2022-07-18 14:29:57 +0100

    remove /

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useEffect } from 'react';
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
 
-import GrassField from '/src/page/GrassField';
-import Teaser from '/src/page/Teaser';
-import Map from '/src/page/Map';
+import GrassField from 'src/page/GrassField';
+import Teaser from 'src/page/Teaser';
+import Map from 'src/page/Map';
 
 function App() {
 
