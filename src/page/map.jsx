cbf03c1a7a3af4d9a888a3462448e7ca300d70b7
Tinnei Pang <tinneipang@gmail.com>
2022-07-18 03:05:34 +0100

    added map page

---
+++ src/page/map.jsx
@@ -0,0 +1,30 @@
+import React, { useRef, useEffect, useState } from 'react';
+import mapboxgl from 'mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidGlubmVpIiwiYSI6ImNsNG1xNGJxMzAwOHQzam1jcTlqd2FtZXUifQ.6OUTbV6SvN668iZOJAoCGQ';
+
+function Map() {
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(-70.9);
+    const [lat, setLat] = useState(42.35);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom
+        });
+    });
+
+    return (
+        <div>
+            <div ref={mapContainer} className="map-container" />
+        </div>
+    );
+}
+
+export default Map;
