23a32dce5ec75739f2554649b1598d8c9c1baaa2
Tinnei Pang <tinneipang@gmail.com>
2022-07-18 09:58:45 +0100

    add src to path

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      ".": path.resolve(__dirname, "src"),
-    },
-  },
+  plugins: [react()]
 })
