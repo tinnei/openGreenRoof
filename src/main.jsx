d6c7b056487334ad0ebf31e2f2662d7c87259818
Tinnei Pang <tinneipang@gmail.com>
2022-07-18 14:16:38 +0100

    added map

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from 'src/App'
-import './src/index.css'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+import './index.css'
+
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
